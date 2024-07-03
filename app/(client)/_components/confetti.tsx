e45025ee8b2fb11c36773920f26a3cae4c151c03
thobenayann <thobena.yann@orange.fr>
2024-07-03 09:56:37 +0200

    remove test

--- app/(client)/_components/confetti.tsx
+++ app/(client)/_components/confetti.tsx
@@ -6,7 +6,7 @@ function Confetti() {
     return (
         // bg-transparent hover:bg-transparent
         <ConfettiButton className='h-[700px] w-40 bg-transparent hover:bg-transparent max-md:hidden'>
-            TEST
+            {' '}
         </ConfettiButton>
     );
 }
