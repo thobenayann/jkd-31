1d51355a5d41cc3849629ed6bdacd953a776185a
thobenayann <thobena.yann@orange.fr>
2024-07-26 13:39:24 +0200

    style: styles page responsivness

--- app/(client)/27-styles/_components/styles-accordeon.tsx
+++ app/(client)/27-styles/_components/styles-accordeon.tsx
@@ -8,15 +8,15 @@ import stylesData from '@/data/27-styles.json';
 
 export default function StylesAccordeon() {
     return (
-        <Accordion type='single' collapsible className='w-3/4'>
+        <Accordion type='single' collapsible className='max-md:mx-4 md:w-3/4'>
             {stylesData.map((style, index) => (
                 <AccordionItem key={index} value={`item-${index}`}>
                     <AccordionTrigger>
-                        <div className='flex items-center space-x-4 '>
+                        <div className='flex items-center space-x-4'>
                             <div className='w-5 h-5 bg-gray-500 text-white flex items-center justify-center rounded-full text-xs'>
                                 {index + 1}
                             </div>
-                            <div className='text-white hover:underline underline-offset-2'>
+                            <div className='text-white md:hover:underline underline-offset-2 max-md:text-sm text-start'>
                                 {style.title}
                             </div>
                         </div>
--- app/(client)/27-styles/page.tsx
+++ app/(client)/27-styles/page.tsx
@@ -4,7 +4,7 @@ import StylesAnimateTitle from './_components/styles-animate-title';
 
 export default function Styles() {
     return (
-        <div className='flex flex-col items-center justify-center pt-40 space-y-10 pb-10'>
+        <div className='flex flex-col items-center justify-center pt-10 md:pt-40 space-y-32 md:space-y-10 pb-10'>
             <StylesAnimateTitle />
             <FadeInWrapper className='w-full flex justify-center'>
                 <StylesAccordeon />
