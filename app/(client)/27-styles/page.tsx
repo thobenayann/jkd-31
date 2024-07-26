f44566f63627461f5093aa28809bab7e9258d13d
thobenayann <thobena.yann@orange.fr>
2024-07-26 13:53:16 +0200

    chore: add delay on fade in accordeon animation

--- app/(client)/27-styles/page.tsx
+++ app/(client)/27-styles/page.tsx
@@ -6,7 +6,7 @@ export default function Styles() {
     return (
         <div className='flex flex-col items-center justify-center pt-10 md:pt-40 space-y-32 md:space-y-10 pb-10'>
             <StylesAnimateTitle />
-            <FadeInWrapper className='w-full flex justify-center'>
+            <FadeInWrapper className='w-full flex justify-center' delay={0.8}>
                 <StylesAccordeon />
             </FadeInWrapper>
         </div>
