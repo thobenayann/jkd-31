688aeea12aa9e3ca5e0d7721f09d7d36aadf3dea
thobenayann <thobena.yann@orange.fr>
2024-09-04 21:40:05 +0200

    chore: Update home title layout for desktop view

--- app/(client)/(home)/_components/home-title.tsx
+++ app/(client)/(home)/_components/home-title.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function HomeTitle() {
     const isDesktop = useMediaQuery('(min-width: 768px)');
     return isDesktop ? (
-        <div className='md:pl-[5%] md:pt-56'>
+        <div className='md:pl-[5%] md:pt-[5%] 2xl:pt-[10%]'>
             {/* <GradualSpacing
                 className='max-md:text-center font-cinzel text-white text-6xl md:text-6xl'
                 text='JKD Self Defense 31'
