f3a1a731a710aea497da14c4ea8e94e6baa6e1d8
thobenayann <thobena.yann@orange.fr>
2024-09-07 10:07:09 +0200

    chore: Mettre à jour l'URL de metadataBase dans layout.tsx

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -81,10 +81,7 @@ export const metadata: Metadata = {
     },
     metadataBase: new URL(
         process.env.VERCEL_PROJECT_PRODUCTION_URL
-            ? `https://${
-                  process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-                  process.env.VERCEL_URL
-              }`
+            ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
             : 'http://localhost:3000'
     ),
 };
