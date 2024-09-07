456185648d0263b8930644523679104be92a124a
thobenayann <thobena.yann@orange.fr>
2024-09-07 10:37:52 +0200

    chore: Mettre à jour l'URL de metadataBase dans layout.tsx et email-template.tsx

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,43 +4,43 @@ export default function sitemap(): MetadataRoute.Sitemap {
     // Entrées statiques
     const staticEntries = [
         {
-            url: `https://${process.env.VERCEL_URL}`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
             lastModified: new Date(2024, 3, 22),
             changeFrequency: 'never' as const,
             priority: 1,
         },
         {
-            url: `https://${process.env.VERCEL_URL}/association`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/association`,
             lastModified: new Date(2024, 8, 14),
             changeFrequency: 'never' as const,
             priority: 0.8,
         },
         {
-            url: `https://${process.env.VERCEL_URL}/tarifs`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/tarifs`,
             lastModified: new Date(2024, 8, 14),
             changeFrequency: 'never' as const,
             priority: 0.8,
         },
         {
-            url: `https://${process.env.VERCEL_URL}/events`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/events`,
             lastModified: new Date(2024, 8, 14),
             changeFrequency: 'monthly' as const,
             priority: 0.8,
         },
         {
-            url: `https://${process.env.VERCEL_URL}/contact`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/contact`,
             lastModified: new Date(2024, 8, 14),
             changeFrequency: 'never' as const,
             priority: 0.8,
         },
         {
-            url: `https://${process.env.VERCEL_URL}/27-styles`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/27-styles`,
             lastModified: new Date(2024, 8, 14),
             changeFrequency: 'never' as const,
             priority: 0.8,
         },
         {
-            url: `https://${process.env.VERCEL_URL}/legal`,
+            url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/legal`,
             lastModified: new Date(2024, 8, 14),
             changeFrequency: 'never' as const,
             priority: 0.8,
--- emails/email-template.tsx
+++ emails/email-template.tsx
@@ -24,7 +24,7 @@ export default function Email({
     const baseUrl =
         process.env.VERCEL_ENV === 'development'
             ? 'http://localhost:3000'
-            : `https://${process.env.VERCEL_URL}`;
+            : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
     return (
         <Tailwind
