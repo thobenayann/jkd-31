45ad48b453e04835380c8278e132f6cf4325a5d3
thobenayann <thobena.yann@orange.fr>
2025-03-14 11:36:26 +0100

    refactor: amélioration de la gestion des éléments lord-icon et ajustement de l'effet de flou
    - Ajout de la gestion dynamique des éléments lord-icon dans le composant LordIcon.
    - Utilisation de useRef pour référencer le conteneur des icônes.
    - Suppression de la marge dans le hook useInView du composant BlurFade.

--- components/shared/lord-icon.tsx
+++ components/shared/lord-icon.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 interface LordIconProps {
     src: string;
@@ -20,25 +20,40 @@ const LordIcon: React.FC<LordIconProps> = ({
     style,
     containerClassName,
 }) => {
+    const iconRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        if (typeof window !== 'undefined') {
+        if (typeof window !== 'undefined' && iconRef.current) {
             import('lottie-web').then((lottie) => {
                 import('@lordicon/element').then((module) => {
                     module.defineElement(lottie.default.loadAnimation);
+
+                    // Créer dynamiquement l'élément lord-icon après le chargement du module
+                    const lordIcon = document.createElement('lord-icon');
+                    lordIcon.setAttribute('src', src);
+                    lordIcon.setAttribute('trigger', trigger);
+                    if (target) lordIcon.setAttribute('target', `#${target}`);
+                    if (colors) lordIcon.setAttribute('colors', colors);
+                    if (style) {
+                        Object.entries(style).forEach(([key, value]) => {
+                            // @ts-ignore
+                            lordIcon.style[key] = value;
+                        });
+                    }
+
+                    // Vider et ajouter l'élément
+                    if (iconRef.current) {
+                        iconRef.current.innerHTML = '';
+                        iconRef.current.appendChild(lordIcon);
+                    }
                 });
             });
         }
-    }, []);
+    }, [src, trigger, target, colors, style]);
 
     return (
         <div className={cn('relative', containerClassName)}>
-            <lord-icon
-                src={src}
-                trigger={trigger}
-                target={`#${target}`}
-                colors={colors}
-                style={style}
-            ></lord-icon>
+            <div ref={iconRef} className='lord-icon-container'></div>
             {/* <a
                 href='https://lordicon.com/'
                 className='invisible absolute top-0 left-0'
